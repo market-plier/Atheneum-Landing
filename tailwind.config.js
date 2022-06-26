ddb36b5b36ea7549999537ef6cc43407fb99e27d
Gleb Maximenko <maksimenkogleb@gmail.com>
2022-06-26 14:39:02 +0300

    Mobile: Introduction done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,60 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-  extend: {},
-  },
-  plugins: [],
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      backgroundImage: {
+        'gradient': 'linear-gradient(258.9deg, #18C8FF -6.52%, #933FFE 91.35%)',
+        'background': 'radial-gradient(62.81% 133.94% at 17.27% 14.86%, #04127B 0%, #00001C 100%)',
+      },
+      colors: {
+        'white': '#FFFFFF',
+        'trans-white': '#FFFFFFCC',
+        'blue': '#5BCEFF',
+
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    corePlugins: {
+      container: false
+    },
+    plugins: [
+      function ({ addComponents }) {
+        addComponents({
+          '.container': {
+            maxWidth: '100%',
+            '@screen sm': {
+              maxWidth: '320px',
+            },
+            '@screen md': {
+              maxWidth: '640px',
+            },
+            '@screen lg': {
+              maxWidth: '768px',
+            },
+            '@screen xl': {
+              maxWidth: '1024px',
+            },
+          }
+        })
+      }
+    ]
+  }
 };
