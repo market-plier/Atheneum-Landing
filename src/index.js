1c336c878c9dfa67de385b61e6dcda67cd301fe8
Hlib.Maksymenko <hmaksymenko@deloitte.com>
2022-06-29 00:10:32 +0300

    Mobile ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import "./style.scss";
+
+import { Carousel } from 'bootstrap';
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ module.exports = {
       backgroundImage: {
         'gradient': 'linear-gradient(258.9deg, #18C8FF -6.52%, #933FFE 91.35%)',
         'background': 'radial-gradient(62.81% 133.94% at 17.27% 14.86%, #04127B 0%, #00001C 100%)',
-        'green': 'radial-gradient(50% 50% at 50% 50%, rgba(0, 194, 78, 0.15) 0%, rgba(34, 208, 150, 0.15) 100%)'
+        'green': 'radial-gradient(50% 50% at 50% 50%, rgba(0, 194, 78, 0.15) 0%, rgba(34, 208, 150, 0.15) 100%)',
+        'sponsor': 'linear-gradient(258.9deg, rgba(24, 200, 255, 0.3) -6.52%, rgba(147, 63, 254, 0.3) 91.35%)',
       },
       colors: {
         'white': '#FFFFFF',
