06811b63a7cf64dcb9cd7f03680b52b428250a89
Hlib.Maksymenko <hmaksymenko@deloitte.com>
2022-06-30 13:23:55 +0300

    Mobile fix

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import "./style.scss";
 
 import { Modal } from 'bootstrap';
 
-var isFirstShowCall = false;
-window.addEventListener('DOMContentLoaded', (_event) => {
-    document.querySelector('#thankYouPopUp').addEventListener('show.bs.modal', function (e) {
-        isFirstShowCall = !isFirstShowCall; // Prevents an endless recursive call
-        if (isFirstShowCall) {
-            e.preventDefault(); // Prevent immediate opening
-            window.setTimeout(function () {
-                const myModalAlternative = new Modal('#thankYouPopUp');
-                myModalAlternative.show();
-            }, 3000)
-        }
-    });
+// var isFirstShowCall = false;
+// window.addEventListener('DOMContentLoaded', (_event) => {
+//     document.querySelector('#thankYouPopUp').addEventListener('show.bs.modal', function (e) {
+//         isFirstShowCall = !isFirstShowCall; // Prevents an endless recursive call
+//         if (isFirstShowCall) {
+//             e.preventDefault(); // Prevent immediate opening
+//             window.setTimeout(function () {
+//                 const myModalAlternative = new Modal('#thankYouPopUp');
+//                 myModalAlternative.show();
+//             }, 3000)
+//         }
+//     });
+// });
+document.querySelector('#closeBtn').addEventListener('click', function () {
+    document.querySelector('.burger-menu').classList.add('opacity-0', 'invisible');
+    document.querySelector('#wrapper').attributeStyleMap.clear();
 });
